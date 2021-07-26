2d0b25edc18ee1cc2721badf20801d076a44dc73
Alvin Lal <alvinzzz2001@gmail.com>
2021-07-27 01:36:00 +0530

    test

--- frontend/src/components/PostCard.js
+++ frontend/src/components/PostCard.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { getUserInfo } from "../Helpers/localStorage";
-import { updateLikes, updateBookmarks } from "../Helpers/apiCalls";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { getUserInfo } from '../Helpers/localStorage';
+import { updateLikes, updateBookmarks } from '../Helpers/apiCalls';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function PostCard({
   id,
@@ -32,7 +32,7 @@ export default function PostCard({
     navigator.clipboard
       .writeText(`${window.location.href}post/${id}`)
       .then(() => {
-        toast.success("Copied to ClipBoard", {
+        toast.success('Copied to ClipBoard', {
           position: toast.POSITION.BOTTOM_CENTER,
           autoClose: 1000,
           hideProgressBar: true,
@@ -43,10 +43,10 @@ export default function PostCard({
   const setBookmark = () => {
     const user = getUserInfo();
     if (!user) {
-      history.push("/signin");
+      history.push('/signin');
     } else {
       if (bookMarked) {
-        updateBookmarks("unBookMark", id).then((response) => {
+        updateBookmarks('unBookMark', id).then((response) => {
           if (response.status === 1) {
             setBookMarked(false);
           } else if (response.status == 0) {
@@ -54,7 +54,7 @@ export default function PostCard({
           }
         });
       } else if (!bookMarked) {
-        updateBookmarks("bookMark", id).then((response) => {
+        updateBookmarks('bookMark', id).then((response) => {
           if (response.status === 1) {
             setBookMarked(true);
           } else if (response.status == 0) {
@@ -68,10 +68,10 @@ export default function PostCard({
   const setLike = () => {
     const user = getUserInfo();
     if (!user) {
-      history.push("/signin");
+      history.push('/signin');
     } else {
       if (liked) {
-        updateLikes("unlike", id).then((response) => {
+        updateLikes('unlike', id).then((response) => {
           if (response.status === 1) {
             setLiked(false);
           } else if (response.status == 0) {
@@ -79,7 +79,7 @@ export default function PostCard({
           }
         });
       } else if (!liked) {
-        updateLikes("like", id).then((response) => {
+        updateLikes('like', id).then((response) => {
           if (response.status === 1) {
             setLiked(true);
           } else if (response.status === 0) {
@@ -93,71 +93,71 @@ export default function PostCard({
   const history = useHistory();
 
   return (
-    <div className="postCard">
+    <div className='postCard'>
       <img
-        className="image"
+        className='image'
         src={imageUrl}
         onClick={() => history.push(`/post/${id}`)}
       />
-      <div className="info">
+      <div className='info'>
         <p>{tags.map((tag) => `#${tag.toUpperCase()} `)}</p>
-        <div className="postTitle" onClick={() => history.push(`/post/${id}`)}>
+        <div className='postTitle' onClick={() => history.push(`/post/${id}`)}>
           {title}
         </div>
-        <div className="subTitle" onClick={() => history.push(`/post/${id}`)}>
+        <div className='subTitle' onClick={() => history.push(`/post/${id}`)}>
           {description}
         </div>
-        <div className="dateAndTools">
-          <div className="date" onClick={() => history.push(`/post/${id}`)}>
+        <div className='dateAndTools'>
+          <div className='date' onClick={() => history.push(`/post/${id}`)}>
             {date} - {author}
           </div>
-          <div className="tools">
-            <div className="toolsBtn">
+          <div className='tools'>
+            <div className='toolsBtn'>
               <img
                 src={
                   liked
-                    ? require("../assets/like.png")
-                    : require("../assets/unlike.png")
+                    ? require('../assets/like.png')
+                    : require('../assets/unlike.png')
                 }
-                alt=""
-                className="likeBtn"
+                alt=''
+                className='likeBtn'
                 onClick={setLike}
               />
             </div>
-            <div className="toolsBtn">
-              <div className="dropDown">
-                <img src={require("../assets/share.png")} alt="" />
-                <div className="shareDropDownContent">
-                  <div className="arrow-up"></div>
-                  <div className="shareDropDownBtn">
+            <div className='toolsBtn'>
+              <div className='dropDown'>
+                <img src={require('../assets/share.png')} alt='' />
+                <div className='shareDropDownContent'>
+                  <div className='arrow-up'></div>
+                  <div className='shareDropDownBtn'>
                     <img
-                      src={require("../assets/facebookShare.png")}
-                      alt=""
-                      className="shareImg"
+                      src={require('../assets/facebookShare.png')}
+                      alt=''
+                      className='shareImg'
                     />
                     <img
-                      src={require("../assets/clipBoard.png")}
-                      alt=""
-                      className="shareImg"
+                      src={require('../assets/clipBoard.png')}
+                      alt=''
+                      className='shareImg'
                       onClick={copyToClipBoard}
                     />
                     <img
-                      src={require("../assets/twitter.png")}
-                      alt=""
-                      className="shareImg"
+                      src={require('../assets/twitter.png')}
+                      alt=''
+                      className='shareImg'
                     />
                   </div>
                 </div>
               </div>
             </div>
-            <div className="toolsBtn">
+            <div className='toolsBtn'>
               <img
                 src={
                   bookMarked
-                    ? require("../assets/bookmarked.png")
-                    : require("../assets/bookmark.png")
+                    ? require('../assets/bookmarked.png')
+                    : require('../assets/bookmark.png')
                 }
-                alt=""
+                alt=''
                 onClick={setBookmark}
               />
             </div>
